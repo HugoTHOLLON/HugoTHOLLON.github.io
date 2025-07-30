89c333b5abf283f9c4fab848545020f65f2c766b
HugoTHOLLON <hugo.thollon@gmail.com>
2025-07-30 12:20:51 +0200

    Initial commit with both new and old header styles

---
+++ assets/js/variablesResize.js
@@ -0,0 +1,17 @@
+function setStyleVariables() {
+  document.querySelectorAll(".crystal-container").forEach((container) => {
+    const width = container.offsetWidth + "px";
+    const height = container.offsetHeight + "px";
+    container.style.setProperty("--banner-width", width);
+    container.style.setProperty("--banner-height", height);
+    console.log("properties changed after resize");
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  window.addEventListener("resize", setStyleVariables);
+  // resize again after 0.7s in case of transitions
+  window.addEventListener("resize", () => setTimeout(setStyleVariables, 700));
+
+  setStyleVariables();
+});
